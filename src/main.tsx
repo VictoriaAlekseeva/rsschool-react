4dbdcb3270468ffedf94822bf0dbf19a31affbd6
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-11-30 19:23:50 +0100

    feat: Add React Hook Form, Yup, Redux Toolkit and React Router to the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import App from './App.tsx';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import './index.css';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* <App /> */}
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
