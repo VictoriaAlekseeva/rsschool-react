86eee39def47a05580afafda351a0d7d2143cb2a
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-12-04 00:01:24 +0100

    refactor: remove comments

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,4 +2,3 @@ import { schema } from './validation';
 import * as yup from 'yup';
 
 export interface IFormInput extends yup.InferType<typeof schema> {}
-// export type IFormInput = yup.InferType<typeof schema>;
